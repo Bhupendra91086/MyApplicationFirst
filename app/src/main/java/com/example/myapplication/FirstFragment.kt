a5cbad05c41ac41b486c4e2546a2e9b37c5d2903
Bhupendra91086 <Bhupendra91086@gmail.com>
2021-03-21 00:01:28 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var anim = AnimationUtils.loadAnimation(this.activity,R.anim.ttb)
+        var textview = view.findViewById(R.id.textview_first) as TextView
+        textview.startAnimation(anim)
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+}
